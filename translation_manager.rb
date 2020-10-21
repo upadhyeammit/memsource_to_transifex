f947c65d1422a2e2dac9559ccc9a9c31f720cf20
Amit Upadhye <upadhyeammit@gmail.com>
2020-10-21 17:03:32 +0530

    Define upload flow for memsource, clean unwanted code

--- translation_manager.rb
+++ translation_manager.rb
@@ -1,151 +1,79 @@
 # frozen_string_literal: true
 require 'clamp'
-require 'rest-client'
-require 'json'
-require 'parallel'
-require 'transifex'
+require './mem_source.rb'
+require './transi_fex'
 
 # Class to handle different commands with respect to this tool
 class TransManager < Clamp::Command
-  option ['-t', '--transifex-to-memsource'], :flag, 'Upload translations from Transifex to Memsource'
+  option ['--date'],'','[MemSource] (YYYY-MM-DD) Project create date on Memsource to selectively upload all projects uploaded on specific date', required: true
+  option ['--lang_codes'],'','Only work on specific languages', default: %w[es fr ja pt_br zh_cn], multivalued: true
   option ['-m', '--memsource-to-transifex'], :flag, 'Upload translations from Memsource to Transifex'
-  parameter '[DATE]', 'DATE', 'Project create date on Memsource to selectively upload all projects uploaded on specific date'
+  option ['--project-template'],'', '[MemSource] Project template id', default: nil
+  option ['--project-name'],'','[TransiFex] Project name', required: true
+  option ['--resource-names'], '', '[TransiFex] Only work on specific resources. Comma separated list', multivalued: true
+  option ['-t', '--transifex-to-memsource'], :flag, 'Upload translations from Transifex to Memsource'
   parameter '[WORK_DIR]', 'Directory to save PO files if there is failure to upload those, useful to correct few bits and bytes', default: "/tmp/translations"
-  parameter '[RESOURCE_NAMES]', 'Only work on specific resources', default: [], multivalued: true
-  parameter '[project_name]', 'Project name for Transifex'
-  parameter '[LANGS]', 'Only work on specific languages', default: %w[ja es fr zh_cn pt_br], multivalued: true
 
   def execute
-    Memsource.new(date, langs, resource_names, work_dir) if transifex_to_memsource?
-    Transifex.new(langs, project_name, resource_names, work_dir) if memsource_to_transifex?
+    @memsource = MemSource.new(date, lang_codes_list, resource_names_list, work_dir)
+    @transifex = TransiFex.new(lang_codes_list, project_name, resource_names_list, work_dir)
+    # create work dir,if not created ask user to backup and clean it
+    if !Dir.exist?(work_dir)
+      puts 'Creating work Directory'
+      Dir.mkdir(work_dir)
+    else
+      raise "Work directory #{work_dir} already exist. Backup or delete the directory manually"
+    end
+    upload_to_memsource if transifex_to_memsource?
+    upload_to_transifex if memsource_to_transifex?
+  end
+
+  def upload_to_memsource
+    memsource_project_uuid = {}
+
+    if !resource_names_list.empty?
+      tr_resources = resource_names_list.first.split(',').sort
+    else
+      tr_resources = @transifex.resources.map {|res| res.fetch('slug')}.sort
+    end
+
+    puts "Creating directories for #{tr_resources}"
+    # Create project directories and languages directories
+    memsource_project_uuid.each do |name, _|
+      project_dir = work_dir+'/'+name
+      Dir.mkdir(project_dir)
+      lang_codes_list.each {|lang| Dir.mkdir(project_dir+'/'+lang)}
+    end
+
+    puts "Downloading translations for #{tr_resources} and languages #{lang_codes_list}"
+    # Download the translation of every lang_codes_list for every resource
+    memsource_project_uuid.each do |name,uuid|
+      lang_codes_list.each do |code|
+        file_path = work_dir+'/'+name+'/'+code+'/'+name+'.po'
+        @transifex.translation(name, TransiFex::LANG_MAP.fetch(code), file_path)
+      end
+    end
+
+    puts "Creating #{tr_resources} projects on MemSource"
+    # Create respective projects on memsource
+    tr_resources.each do |res|
+      project_uuid = @memsource.create_project(res, project_template || MemSource::PROJECT_TEMPLATE)
+      memsource_project_uuid[res] = project_uuid
+    end
+
+    puts "Uploading translations for #{tr_resources}"
+    # Upload the translations to memsource
+    memsource_project_uuid.each do |name, uuid|
+      lang_codes_list.each do |code|
+        file_path = work_dir+'/'+name+'/'+code+'/'+name+'.po'
+        file_content = File.open(file_path,'r')
+        @memsource.upload_locale(uuid, file_content, name+'.po', TransiFex::LANG_MAP.fetch(code))
+      end
+    end
+  end
+
+  def upload_to_transifex
   end
 end
 
 TransManager.run
-
-# A placeholder for common methods for Memsource and Transifex
-module Common
-  def parse_json(body)
-    JSON.parse(body)
-  end
-
-  def auth
-    parse_json(File.read('auth.json'))
-  end
-
-  def pot_files
-    Parallel.each(id_name, in_threads: 25) do |resource|
-      jobs = get_jobs(resource['id'])
-      Parallel.each(jobs, in_threads: 10) do |job|
-        response = RestClient.get "#{@mem_api_url}projects/#{resource['id']}/jobs/#{job['uid']}/targetFile/?token=#{token}"
-        puts "Saving file for resource #{resource['name']} and lang #{job['targetLang']}"
-        # trim first two and last one line
-        write_tx(resource['name'], job['targetLang'], response.body.lines[3..-2].join)
-      end
-    end
-  end
-
-  def work_dir_pot_files
-    Parallel.each(Dir.children(@work_dir), in_threads: 12) do |resource|
-      Parallel.each(Dir.children("#{@work_dir}/#{resource}"), in_threads: 5) do |lang|
-        file_name = "#{@work_dir}/#{resource}/#{lang}"
-        content = File.new(file_name)
-        write_tx(resource,lang,content.read)
-      end
-    end
-  end
-end
-
-
-# Class for all memsource releated vars and methods
-class Memsource
-  MEMSOURCE_API_URL = 'https://cloud.memsource.com/web/api2/v1/'
-  DOMAIN_NAME = 'Satellite'
-
-  def initilize(date, project_names, work_dir)
-    @project_upload_date = date #yyy-mm-dd
-    @project_names = project_names # this should be array
-    @work_dir = work_dir
-    @resouce = RestClient::Resource.new(MEMSOURCE_API_URL.to_s, headers: { content_type: 'application/json' })
-  end
-
-  # @mem_api_url = 'https://cloud.memsource.com/web/api2/v1/'
-  def token
-    payload = { 'userName' => auth['memsource']['username'], 'password' => auth['memsource']['password'] }.to_json
-    response = @resouce['auth/login'].post(payload)
-    @token ||= parse_json(response)['token']
-  end
-
-  def id_name
-    keys = %w[id name]
-    response = RestClient.get "#{@mem_api_url}projects/?token=#{token}", { params: { 'pageSize' => 50, 'domainName' => 'Satellite' } }
-    projects ||= parse_json(response)['content']
-    Parallel.map(projects, in_threads: 25) do |p|
-      if p['createdBy']['userName'] == auth['memsource']['username'] && p['dateCreated'] =~ @project_upload_date
-        p.select { |k, _v| keys.include? k }
-      end
-    end.compact
-  end
-
-  def get_jobs(pid)
-    keys = %w[targetLang uid status]
-    response = RestClient.get "#{@mem_api_url}projects/#{pid}/jobs/?token=#{token}"
-    jobs ||= parse_json(response)['content']
-    Parallel.map(jobs, in_threads: 10) do |j|
-    j.select { |k, _| keys.include? k }
-    end.compact
-  end
-end
-
-# Class for all vars and methods for Transifex
-class Transifex
-  @tran_api_url = 'https://www.transifex.com/api/2/'
-  
-  def init(langs, project_name, resource_names, work_dir)
-    Transifex.configure do |c|
-      c.client_login = auth['transifex']['username']
-      c.client_secret = auth['transifex']['password']
-    end
-    @project = project_name 
-    @resource_names = resource_names
-    @work_dir = work_dir
-    @langs = langs
-  end
-
-  def project
-    Transifex::Project.new(@project)
-  end
-
-  def resources(name = [])
-    return project.resource(name) if !name.empty?
-    
-    project.resources.fetch
-  end
-
-  def translation(resource, lang, with_file = false)
-    return resource.translation(lang).fetch_with_file if with_file
-    
-    resource.translation(lang).fetch
-  end
-
-  def write_tx(r_name, lang, content)
-    lang_map = { 'ja' => 'ja', 'es' => 'es', 'fr' => 'fr', 'zh_cn' => 'zh_CN', 'pt_br' => 'pt_BR' }
-    @project = Transifex::Project.new('foreman')
-    options = { :i18n_type => "PO", :content => content }
-    begin
-      @project.resource(r_name).translation(lang_map[lang]).update(options)
-    rescue StandardError => e
-      puts e.message
-      puts "** Failed to upload translation for project #{r_name} and lang #{lang}"
-      if @mode == 'memsource'
-        unless Dir.exist?("#{@work_dir}/#{r_name}")
-          Dir.mkdir("#{@work_dir}/#{r_name}")
-        end
-        puts "** Writing file to #{@work_dir}/#{r_name}/#{lang} for investigation"
-        f = File.new("#{@work_dir}/#{r_name}/#{lang}", 'w')
-        f.write(content)
-      end
-    end
-  end
-end
-
