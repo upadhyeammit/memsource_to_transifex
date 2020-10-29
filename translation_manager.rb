a3e50ecf60a2bb97010df412e8e9713494296c2d
Amit Upadhye <upadhyeammit@gmail.com>
2020-10-29 17:18:31 +0530

    Move common code, fix transifex workflow

--- translation_manager.rb
+++ translation_manager.rb
@@ -2,7 +2,6 @@
 require 'clamp'
 require './mem_source.rb'
 require './transi_fex'
-
 # Class to handle different commands with respect to this tool
 class TransManager < Clamp::Command
   option ['--date'],'','[MemSource] (YYYY-MM-DD) Project create date on Memsource to selectively upload all projects uploaded on specific date', required: true
@@ -15,15 +14,11 @@ class TransManager < Clamp::Command
   parameter '[WORK_DIR]', 'Directory to save PO files if there is failure to upload those, useful to correct few bits and bytes', default: "/tmp/translations"
 
   def execute
+    Common::create_work_dir(work_dir)
     @memsource = MemSource.new(date, lang_codes_list, resource_names_list, work_dir)
     @transifex = TransiFex.new(lang_codes_list, project_name, resource_names_list, work_dir)
     # create work dir,if not created ask user to backup and clean it
-    if !Dir.exist?(work_dir)
-      puts 'Creating work Directory'
-      Dir.mkdir(work_dir)
-    else
-      raise "Work directory #{work_dir} already exist. Backup or delete the directory manually"
-    end
+
     upload_to_memsource if transifex_to_memsource?
     upload_to_transifex if memsource_to_transifex?
   end
@@ -40,9 +35,9 @@ class TransManager < Clamp::Command
     puts "Creating directories for #{tr_resources}"
     # Create project directories and languages directories
     memsource_project_uuid.each do |name, _|
-      project_dir = work_dir+'/'+'transifex'+'/'+name
-      Dir.mkdir(project_dir)
-      lang_codes_list.each {|lang| Dir.mkdir(project_dir+'/'+lang)}
+      project_dir = @transifex.work_dir+'/'+name
+      Common::create_work_dir(project_dir)
+      lang_codes_list.each {|lang| create_work_dir(project_dir+'/'+lang)}
     end
 
     puts "Downloading translations for #{tr_resources} and languages #{lang_codes_list}"
@@ -51,7 +46,7 @@ class TransManager < Clamp::Command
     # Download the translation of every lang_codes_list for every resource
     memsource_project_uuid.each do |name,uuid|
       lang_codes_list.each do |code|
-        file_path = work_dir+'/'+'transifex'+'/'+name+'/'+code+'/'+name+'.po'
+        file_path = @transifex.work_dir+'/'+name+'/'+code+'/'+name+'.po'
         @transifex.translation(name, TransiFex::LANG_MAP.fetch(code), file_path)
       end
     end
@@ -67,7 +62,7 @@ class TransManager < Clamp::Command
     # Upload the translations to memsource
     memsource_project_uuid.each do |name, uuid|
       lang_codes_list.each do |code|
-        file_path = work_dir+'/'+'transifex'+'/'+name+'/'+code+'/'+name+'.po'
+        file_path = @transifex.work_dir+'/'+name+'/'+code+'/'+name+'.po'
         file_content = File.open(file_path,'r')
         @memsource.upload_locale(uuid, file_content, name+'.po', TransiFex::LANG_MAP.fetch(code))
       end
@@ -77,22 +72,19 @@ class TransManager < Clamp::Command
   def upload_to_transifex
     # The date is required because there can be multiple projects on memsource with same name uploaded on different dates. Which will have duplicate resources handling. The date is easy way to work on only latest translations from memsource
     # Need to change date format to regex to use with memsource api
-    regx_date = '/#{date}/'
-    if !resource_names_list.empty?
-      tr_resources = resource_names_list.first.split(',').sort
-    else
-      tr_resources = @memsource.
-      transifex.resources.map {|res| res.fetch('slug')}.sort
-    end
+    regx_date = Regexp.new date
 
     # The memsource api returns file in po format, so we dont save it on disk before upload
     project_ids_names = @memsource.project_ids_names(regx_date)
-    project_names.each do |project|
-      jobs = @memsource.get_jobs(project['id'])
+    if project_ids_names.empty?
+      raise "Can't find any projects matching with #{date} filter"
+    end
+    project_ids_names.each do |project|
+      jobs = @memsource.get_jobs(project.fetch('id'))
       jobs.each do |job|
-        translated_file = @memsource.pot_file(project['id'], job['uuid'])
-        puts "Uploading file for project #{project['name']} and for lang #{job['targetLang']}"
-        @transifex.write_tx(project['name'], job['targetLang'], translated_file, work_dir+'/'+'transifex')
+        translated_file = @memsource.pot_file(project.fetch('id'), job.fetch('uid'))
+        puts "Uploading file for project #{project.fetch('name')} and for lang #{job.fetch('targetLang')}"
+        @transifex.write_tx(project.fetch('name'), job.fetch('targetLang'), translated_file)
       end
     end
   end
