3bc4ee70f33be23a02848d80d7042a1fb4155b96
Amit Upadhye <upadhyeammit@gmail.com>
2020-10-30 19:29:53 +0530

    Remove work_dir_pot_files method

--- common.rb
+++ common.rb
@@ -8,21 +8,11 @@ module Common
     parse_json(File.read('auth.json'))
   end
 
-  def self.work_dir_pot_files
-    Parallel.each(Dir.children(@work_dir), in_threads: 12) do |resource|
-      Parallel.each(Dir.children("#{@work_dir}/#{resource}"), in_threads: 5) do |lang|
-        file_name = "#{@work_dir}/#{resource}/#{lang}"
-        content = File.new(file_name)
-        write_tx(resource,lang,content.read)
-      end
-    end
-  end
-
-  def self.create_work_dir(name)
+  def self.create_work_dir(name,filesystem = false)
     if !Dir.exist?(name)
       puts "Creating work directory for #{name}"
       Dir.mkdir(name)
-    else
+    elsif !filesystem
       raise "Work directory #{name} already exist. Backup or delete the directory manually"
     end
   end
